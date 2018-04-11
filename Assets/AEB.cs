2372a5909e9c34e5168312959a2347a67bb109a9
Ben Tristem <github@bentristem.com>
2018-04-11 18:42:38 +0100

    Car brakes at fixed distance

--- Assets/AEB.cs
+++ Assets/AEB.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
 
 public class AEB : MonoBehaviour {
 
     [SerializeField] Radar frontBumperRadar;
 
+    CarController carController;
+    Rigidbody myRigidBody;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        carController = GetComponent<CarController>();
+        myRigidBody = GetComponent<Rigidbody>();
+
+        myRigidBody.velocity = 20f * Vector3.forward;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        print(frontBumperRadar.GetDistance()); 
-	}
+	void Update ()
+    {
+        bool drivingForward = myRigidBody.velocity.z > 0;
+        if (!drivingForward) { return; }
+
+        float distanceToObstacle = frontBumperRadar.GetDistance();
+        print("Obstacles detected at: " + distanceToObstacle);
+        if (distanceToObstacle > 70f) // todo parameterise
+        {
+            carController.Move(0, 1f, 0, 0);
+        }
+        else if (distanceToObstacle < 40f)
+        {
+            carController.Move(0, 0, -1f, 0);
+        }
+    }
 }
