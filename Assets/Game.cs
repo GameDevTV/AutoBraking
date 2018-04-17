eaf3fe2f713913632f43ca14d2fd51e0d8418ec2
Ben Tristem <github@bentristem.com>
2018-04-17 13:55:59 +0100

    Timescale and score exposed

---
+++ Assets/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    [SerializeField] [Range(1f, 10f)] float timeScale = 1f;
+    [SerializeField] float distanceRemaining;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Time.timeScale = timeScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StoppedWithDistanceRemaining(float distanceRemaining)
+    {
+        this.distanceRemaining = distanceRemaining;
+    }
+}
