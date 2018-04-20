b45c66adcdaacc4ba09539dd1e001a8caf40f02f
Ben Tristem <github@bentristem.com>
2018-04-20 12:21:11 +0100

    Got code in right place and writing to struct

---
+++ Assets/BufferControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BufferControl : MonoBehaviour {
+
+    CarStats carStats = new CarStats();
+
+	// Use this for initialization
+	void Start ()
+    {
+        carStats.Speed = 10;
+        print(carStats.Speed);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
