80bf1fcd814697bfacb93c48c87b45d151dee8d6
Ben Tristem <github@bentristem.com>
2018-04-20 12:56:39 +0100

    Bytes on a wire

--- Assets/BufferControl.cs
+++ Assets/BufferControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Google.Protobuf;
+using System;
 
 public class BufferControl : MonoBehaviour {
 
@@ -11,14 +12,20 @@ public class BufferControl : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        carStats.Speed = 10;
-        print(carStats.Speed);
         localSocket = new LocalSocket();
+
+        carStats.Speed = Int32.MaxValue;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        var bytesToSend = carStats.ToByteArray().Length;
+        // todo encode message length in bytes as 16 or 32 bit int
+        // add this length to the start of the byte stream
         localSocket.Send(carStats.ToByteArray());
+        print(bytesToSend);
+        // on decode read this first, to know how much message to read
+        // then decode
 	}
 }
